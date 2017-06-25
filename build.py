2620b925703e7324265323cfa96d07a43bb6bb23
birajparikh16 <birajparikh16@gmail.com>
2017-06-25 19:25:19 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-
 def solution(array):
-    """
-    Enter your code here
-    """
+
+    a = np.array(array)
+    return a.astype(float)
+
+# solution([1, 2, 3, 4])
+# Output : array([ 1.,  2.,  3.,  4.])
